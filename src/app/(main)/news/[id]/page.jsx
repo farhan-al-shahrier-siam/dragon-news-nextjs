42d253f5a7904962478745cb2d821954532dd37a
farhan-al-shahrier-siam <farhanalshahriersiam@gmail.com>
2026-04-28 19:18:45 +0600

    NewsDetails Page is done as per figma

--- src/app/(main)/news/[id]/page.jsx
+++ src/app/(main)/news/[id]/page.jsx
@@ -1,9 +1,58 @@
-import React from 'react';
+import { getNewsDetailsById } from "@/lib/data";
+import Image from "next/image";
+import Link from "next/link";
+import { CiBookmark, CiShare2 } from "react-icons/ci";
+import { FaEye, FaStar } from "react-icons/fa";
 
-const NewsDetailsPage = () => {
+const NewsDetailsPage = async ({ params }) => {
+    const { id } = await params;
+
+    const news = await getNewsDetailsById(id);
+    console.log(news);
+
     return (
-        <div>
-            news details page
+        <div className="max-w-5xl mx-auto my-8">
+            <div className="card bg-base-100 shadow-sm">
+                <div className="card-body">
+                    <div className="flex items-center justify-between bg-slate-200 p-4">
+                        <div className="flex gap-1 items-center">
+                            <Image className="rounded-full" src={news.author?.img} alt={news.author.name} height={40} width={40} />
+                            <div>
+                                <h2 className="font-semibold">{news.author?.name}</h2>
+                                <h2 className="text-xs">{news.author?.published_date}</h2>
+                            </div>
+                        </div>
+                        <div className="flex items-center justify-between">
+                            <CiShare2 className="text-2xl" />
+                            <CiBookmark className="text-2xl" />
+                        </div>
+                    </div>
+
+                    <h2 className="card-title">{news.title}</h2>
+                    <figure>
+                        <Image className="w-full" src={news.image_url} alt={news.title} height={300} width={300} />
+                    </figure>
+                    <p className="">{news.details}</p>
+                    <div>
+                        <Link href={`/category/${news.category_id}`}>
+                            <button className="text-orange-400 font-semibold cursor-pointer">All news in this category</button>
+                        </Link>
+                        <div className="flex items-center justify-between border-t-2 border-gray-500 pt-2 mt-4">
+                            <div className="flex items-center justify-between gap-1">
+                                <FaStar className="text-xl text-orange-400" />
+                                <FaStar className="text-xl text-orange-400" />
+                                <FaStar className="text-xl text-orange-400" />
+                                <FaStar className="text-xl text-orange-400" />
+                                <p>{news.rating?.number}</p>
+                            </div>
+                            <div className="flex items-center justify-between gap-2 font-bold">
+                                {" "}
+                                <FaEye /> {news.total_view}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/lib/data.js
+++ src/lib/data.js
@@ -3,8 +3,15 @@ export const getCategories = async () => {
     const data = await res.json();
     return data.data;
 };
+
 export const getNewsByCategoryId = async (category_id) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/news/category/${category_id}`);
     const data = await res.json();
     return data.data;
+};
+
+export const getNewsDetailsById = async (news_id) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/news/${news_id}`);
+    const data = await res.json();
+    return data.data[0];
 };
