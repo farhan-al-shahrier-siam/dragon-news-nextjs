c0abfdaa691e5a43c9c0335f273c8b638a3278d5
farhan-al-shahrier-siam <farhanalshahriersiam@gmail.com>
2026-04-29 19:25:32 +0600

    Login with google and github is functional now

--- src/components/Homepage/news/RightSideBar.jsx
+++ src/components/Homepage/news/RightSideBar.jsx
@@ -1,14 +1,27 @@
+"use client";
+import { authClient } from "@/lib/auth-client";
 import { FaGithub, FaGoogle } from "react-icons/fa";
 
 const RightSideBar = () => {
+    const handleGoogleSignin = async () => {
+        const data = await authClient.signIn.social({
+            provider: "google",
+        });
+    };
+    const handleGithubSignin = async () => {
+        const data = await authClient.signIn.social({
+            provider: "github",
+        });
+    };
+
     return (
         <div>
             <h2 className="font-bold text-lg mb-6">Login With</h2>
             <div className="flex flex-col gap-2">
-                <button className="btn text-blue-500 border-blue-500">
+                <button onClick={handleGoogleSignin} className="btn text-blue-500 border-blue-500">
                     <FaGoogle /> Login with Google
                 </button>
-                <button className="btn">
+                <button onClick={handleGithubSignin} className="btn ">
                     <FaGithub />
                     Login with Github
                 </button>
